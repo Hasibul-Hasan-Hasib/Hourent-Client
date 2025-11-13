300ee151f64ee85e313600110577ca30748374d6
Hasibul Hasan Hasib <mdhasibulctg100@gmail.com>
2025-11-14 00:58:04 +0600

    feat: update index.html title and import global styles in main.jsx for improved branding and styling consistency

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { router } from './routes/routes';
 import AuthProvider from './providers/AuthProvider';
+import './globals.css';
 
 const queryClient = new QueryClient();
 
